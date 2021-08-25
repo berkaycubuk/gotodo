6a400418faba3cfaa15eaaf0990e89737a45866e
Berkay Çubuk <berkaycubuk@gmail.com>
2021-08-25 19:48:43 +0300

    / route says welcome

--- main.go
+++ main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func welcome(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", welcome)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
 func main() {
+	handleRequests()
 }
